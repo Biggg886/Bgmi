a6ac10ac479f5688c1754941ab9b09d052b3168f
Biggg886 <154685671+Biggg886@users.noreply.github.com>
2024-09-01 10:59:15 +0700

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7301648378:AAHZt8SOGb72ivP0RQBPeRQQvDq-oyRAo9E'
-ADMIN_ID = '2022961119'
+API_TOKEN = '7020534774:AAFYHmLHrz9plmifsYp9QzB7g-JaIkkLGCw'
+ADMIN_ID = '7094215368'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
